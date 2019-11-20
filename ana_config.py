0d4f9c4e015ac61c1aea958805fea92d08ed457d
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2019-11-20 07:44:34 +0100

    Update ana_config.py

--- ana_config.py
+++ ana_config.py
@@ -1,17 +1,49 @@
-res_folder = "Result" # Folder for results
-res_folder = "Today" # Folder for results
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 11:41:34 2019
+
+@author: Stolar
+"""
+import os
+import astropy.units as u
+# Suppress RunTimeWarning in mc_3d background fitting
+import sys
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
 
 #------------------------------------------------------------------------------
 ### Code Initialisations
 #------------------------------------------------------------------------------
-dbg_level = 0 # 0 : event counting, 1: event counting + some results, 2: + details for each event, 3: + plots 
-showplots = 2 # 0 : not shown, not written, 1: shown on screen, 2 : shown on screen and written out
-
+dbg_level = 0 # 0 : evt counting, 1: evt counting + some results, 2: + details for each evt, 3: + plots
+showplots = 2 # 0 : not shown, not written,
+              # 1 : shown on screen
+              # 2 : shown on screen and written out
+              # 3 : written out, not shown on screen (for production)
+likelihood = 2 # If zero, do the on-off analysis, otherwise set the degree fof freedom
 #------------------------------------------------------------------------------
 ### Simulation Initialisations
 #------------------------------------------------------------------------------
 niter = 10 # Number of iterations for the Monte Carlo
 alpha = 0.2 # 5 zones (1/0.2) to define the off region for background estimation
+binsize  = 0.125*u.deg
+fov      = 5.*u.deg # full width (i.e. if centered in zero, -fov/2, fov/2)
+# for quick tests
+#binsize  = 0.5*u.deg
+#fov      = 2.5*u.deg # full width (i.e. if centered in zero, -fov/2, fov/2)
+#------------------------------------------------------------------------------
+### Input
+#------------------------------------------------------------------------------
+res_folder = "../output/Result" # Folder for results
+res_folder = "../output/Today" # Folder for results
+irf_folder = "../input/irf"
+grb_folder = '../input/lightcurves/long_grb_test/' # to be changed into a repository to be scanned
+
+os.environ['GAMMAPY_EXTRA'] =r'../input/gammapy-extra-master'
+os.environ['GAMMAPY_DATA'] =r'../input/gammapy-extra-master/datasets'
+#print(os.getenv('GAMMAPY_EXTRA'))
+#print(os.listdir(os.getenv('GAMMAPY_EXTRA')))
 #------------------------------------------------------------------------------
 ### Physics Initialisations
 #------------------------------------------------------------------------------
@@ -19,17 +51,17 @@ det_level       = 0.9 # Will be detected if n=3,5 sigma reached in det_level of 
 #redfactor       = [1, 2, 3, 5, 10, 20, 50, 100] # Initial flux is divided by this value, for test
 #redfactor       = [0.5,1, 2] # Initial flux is divided by this value, for test
 redfactor = [1]
-
-grb_repository  = 'data/long_grb_test/' # to be changed into a repository to be scanned
 #grblist         = ["LGRB1","LGRB2","LGRB3","LGRB4","LGRB5","LGRB6","LGRB7","LGRB8","LGRB9","LGRB10"]
+grblist         = ["LGRB1","LGRB2","LGRB3","LGRB4","LGRB5"]
 grblist         = ["LGRB1"]
-
 EBLmodel        = "dominguez"
 
 #------------------------------------------------------------------------------
 ### Instrument response functions
 #------------------------------------------------------------------------------
-irf_repository = "irf"
+site        ='Roque de los Muchachos'
+obstime     = '2000-01-01 22:00:04'
+
 chain       = ["Reco1"] # or Reco2
 hemisphere  = ["North"] # or South
 #hemisphere  = ["South"] # or South
