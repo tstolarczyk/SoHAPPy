194b02d7553d50734f8f4f2b2b56bcd73de8fd8b
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2020-08-28 17:15:08 +0200

    Update ana_config.py

--- ana_config.py
+++ ana_config.py
@@ -4,63 +4,91 @@ Created on Tue Jan 22 11:41:34 2019
 
 @author: Stolar
 """
-import os
 import astropy.units as u
+"""
+This module contains most of the steering parameters of SoHAPPy.
+"SoHAPPy -h" in a shell gives the list of parameters that can be superseded
+on the command line 
+"""
 
-#------------------------------------------------------------------------------
-### Physics Initialisations
-#------------------------------------------------------------------------------
-ngrb            = 1 # Number of GRB to be read
-ifirst          = 913 # First GRB to be read
-det_level       = 0.9 # Will be detected if n=3,5 sigma reached in det_level of the population
-#redfactor       = [1, 2, 3, 5, 10, 20, 50, 100] # Initial flux is divided by this value, for test
-#redfactor       = [0.5,1, 2] # Initial flux is divided by this value, for test
-redfactor = [1]
+#-----------------------------------------------------------------------------#
+# These values can be superseded by the command line
+niter  = 100 # Number of iterations for the Monte Carlo
+ngrb   = 1 # Number of GRB to be read - if 1 specific studies on one grb
+
+# First GRB - can be a list then ngrb is useless
+# Prompt
+#ifirst = [2, 6, 12, 30, 139, 172, 191, 278, 490, 506] # Prompt test files 
+#ifirst=12
+
+# Afterglows
+ifirst=457 # Remains at very low altitude in North - good to check altmin
+ifirst = 6   # Seen in North, not seen in South
+ifirst = 85
+#ifirst=3
+#ifirst = [54,416,457,926] # This ones have large slice number in N and S
+#ifirst=901 # Nice overlap
+#ifirst=204 
+#ifirst = [11, 12]
+#ifirst = [188,306,458,454,765,709]
+#ifirst = 57  # Not detected N and S separately. A few slices only -> Check N+S
+#ifirst = 10  # Not detected N and S separately. A few slices only
+#ifirst = 188 # Mammuth, detected the second day also, large site overlap 
+#ifirst = 13  # Not detected
+#ifirst=64 # Detected in N and S with very few slices - good for debug !!!
+#ifirst = 815 # By far the brightest one, not seen in South
+#ifirst = 398 # 5 sigma detected in North, 29 slices. Not visible in South
+#ifirst =399 # Not visible in North nor in South
+#ifirst = 54
+#ifirst = 913
+
+# Debugging : if negative or zero : no plot
+# 0: evt counting, 1: + some results, 2: + event details
+# 3: Plots for each trials - create an animated gif - heavy
+dbg    = 1
+
+
+# prompt tests
+# redshift = 4.0 # Prompt test
+# res_dir    = "D:/000_Today/prompt-visible-nofluctuation-z"+str(redshift)
+
+res_dir    = "D:/000_Today/testnew"
+#-----------------------------------------------------------------------------#
+# Physics
 EBLmodel        = "dominguez"
-#------------------------------------------------------------------------------
-### Simulation Initialisations
-#------------------------------------------------------------------------------
-niter  = 10 # Number of iterations for the Monte Carlo
-alpha  = 0.2 # 5 zones (1/0.2) to define the off region for background estimation
-fov    = [5.*u.deg, 0.125*u.deg] # Full Field-of-view and bin size
-dtslew = [30*u.s, True] # Time to point the GRB,  If False, random < dt_slew
-# for quick tests
-#binsize  = 0.5*u.deg
-#fov      = 2.5*u.deg # full width (i.e. if centered in zero, -fov/2, fov/2)
-#------------------------------------------------------------------------------
-### Code Initialisations
-#------------------------------------------------------------------------------
-dbg_level = 0  # 0 : evt counting, 1: evt counting + some results, 2: + details for each evt, 3: + plots
-showplots = 3 # 0 : not shown, not written,
-               # 1 : shown on screen
-               # 2 : shown on screen and written out
-               # 3 : written out, not shown on screen (for production)
-lkhd      = 0 # If zero, do the on-off analysis, otherwise set the degree of freedom
-#------------------------------------------------------------------------------
-### Input
-#------------------------------------------------------------------------------
-res_folder = "../output/Result" # Folder for results
-res_folder = "../output/Today" # Folder for results
-#res_folder = "../output/Prod10_nobug"
-
+#-----------------------------------------------------------------------------#
+# Detection
+dtslew    = 0*u.s # Time delay to point the GRB, usually 30*u.s 
+fixslew   = True   # If False, random < dt_slew
+dtswift   = 0*u.s # Mean SWIFT latency : mean value is 77*u.s
+fixswift  = True   # If False, the latency is generated from real data file
+swiftfile = "../input/swift/Swift_delay_times.txt" # Real data file
+altmin    = 10*u.deg # Minimum altitude (original default is 10 degrees)
+#-----------------------------------------------------------------------------#
+# Analysis
+lkhd      = 0     # If zero : on-off analysis; otherwise degrees of freedom
+obs_point = "end" # Define the position of observation in the time slice
+#-----------------------------------------------------------------------------#
+# Development
+silent         = False  # If True, nothing written on screen (output to log)
+save_simu      = False # (False) Simulation svaed to file for offline use
+save_grb       = False # (False) GRB saved to sik -> use grb.py main
+write_slices   = False # (False) Store detailed information on slices if True
+signal_to_zero = False # (False) Keep only background, set signal to zero
+do_fluctuate   = False # (True) Statistical fluctuations are enabled 
+do_accelerate  = True # (True) Simulation skipped if no detection in first 10%  
+fixed_zenith   = False # If set to a value ("20deg") freeze zenith in IRF 
+remove_tarred  = False # Remove tarred files, otherwise keep for faster access
+test_prompt    = False # (False) If True test prompt alone (experimental)
+use_afterglow  = False # Prompt characteritics from the afterglow with same id.
+get_visibility = True  # (True) If True, visibility windows are recomputed
+day_after      = 0   # Add up to that days (if negative only that day) 
+#-----------------------------------------------------------------------------#
+# Input-Ouput
 old_file   = False # Use old GRB file
-grb_oldfolder = '../input/lightcurves/long_grb_test/' # to be changed into a repository to be scanned
-grb_folder    = '../input/lightcurves/LONG_FITS'
-os.environ['GAMMAPY_EXTRA'] =r'../input/gammapy-extra-master'
-os.environ['GAMMAPY_DATA'] =r'../input/gammapy-extra-master/datasets'
-#print(os.getenv('GAMMAPY_EXTRA'))
-#print(os.listdir(os.getenv('GAMMAPY_EXTRA')))
+grb_olddir = '../input/lightcurves/long_grb_test/'
+grb_dir    = '../input/lightcurves/LONG_FITS/'
+irf_dir    = "../input/irf/OnAxis/"
+datafile   = "data.txt"
+logfile    = "analysis.log"
 
-
-#------------------------------------------------------------------------------
-### Instrument response functions
-#   Default values to force the source observation conditions
-#------------------------------------------------------------------------------
-fixed       = False # Unused - If True the source observation conditions are fixed
-irf_folder  = "../input/irf"
-#chain       = ["Reco1"] # or Reco2 (Reco1 = eventdisplay, Reco2 = MARS)
-#hemisphere  = ["North"] # or South
-#zenith      = ["20deg"] # ["20deg","40deg","60deg"]
-#azimuth     = ["average"]  # or N, or S
-#duration    = ["100s"] # or 30m, 05h 50h
-#NSB         = False # if True try to find a high NSB file
