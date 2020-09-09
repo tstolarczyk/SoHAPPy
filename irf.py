40825b807dfb8705631f873c37813502253ec22e
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2020-09-09 10:32:17 +0200

    Update irf.py
    Changes in documentation, implement new zenith angle validity domains and create new observation time validity domains (log scale instead of closest in linear time). Chnage naming of theta and phi in more explicit zenith and azimuth. Ther is no default site given to get_irf_file.

--- irf.py
+++ irf.py
@@ -1,8 +1,73 @@
 # -*- coding: utf-8 -*-
+
 """
 Created on Thu Dec 12 09:01:41 2019
 
 @author: Stolar
+
+This module hosts the functionnalities to choose the best IRF from the available
+files and the recipes on their validity domains in observation duration and
+angles.
+
+The recommandations for prod3v2 IRF are the following (Information from
+G. Maier obtained by I. Sadeh on January 30th, 2020).
+
+Angular "interpolation"
+----------------------
+
+* Zenith
+The zenith windows should be defined following a 1/cos(theta) law, which means
+(Iftach says) that depending on the zenith angle the following IRF should be
+used :
+    - zen < 33        : "20deg"
+    - 33 <= zen < 54  : "40deg"
+    - zen > 54        : "60deg"
+
+On top of this, it is foreseen that the instrument performance will not be
+reliable above 66 degrees as implicitly mentionned in A-PERF-0720
+(*Observable Sky: The system as a whole must be able to target any astrophysical
+object in the sky which has an elevation >24 degrees*).
+
+* Azimuth:
+It is recommended to use the average azimuth (not a step function going when
+using the North and South IRF).
+
+Energy thresholds
+-----------------
+Although events should be generated from the minimum allowed value for a given
+IRF, the analysis should restrict to a higher threshold, in order to avoid
+cutoff effects.
+
+Minimum *generated* energies are the following:
+    - 20deg —> E_gen >= 12.6 GeV
+    - 40deg —> E_gen >= 26.4 GeV
+    - 60deg —> E_gen >= 105.3 GeV
+
+Minimum *reconstructed* energies are the following (Iftach suggests these
+values but does not say it this is his conclusion or more generally accepted
+values):
+    - 20deg —> E_grb >= 30 GeV
+    - 40deg —> E_grb >= 40 GeV
+    - 60deg —> E_grb >= 110 GeV
+This corresponds approximately to the generated energies to which one standard
+deviation has been added (the expected energy resolution at low energies is
+deltaE/E (68%) = 0.25.
+
+Note that the CTA performance plots have a cutoff of E>20 GeV.
+Requirements expect the IRF to be reliable above 30 GeV.
+
+
+older IRF files
+---------------
+IRF older than prodv3b2 can be used. They essentially differ in the angular
+resolution at high energies. They include both full arrays, threshold arrays
+and LST-only IRFs. They cover 20 and 40 degrees but not 60 degrees.
+Some 20 degree IRF have 5x 5North and South) or 30x the nominal NSB.
+
+See here :
+https://forge.in2p3.fr/projects/cta_analysis-and-simulations/wiki/Prod3b_based_instrument_response_functions
+And download there : https://forge.in2p3.fr/attachments/download/55554
+
 """
 import os
 import sys
@@ -17,93 +82,83 @@ with warnings.catch_warnings():
     from gammapy.cube import make_map_exposure_true_energy, make_map_background_irf
     from gammapy.cube import PSFKernel
 
-import ana_config as cf
-from utilities import warning, failure, success, highlight,banner
+from utilities import warning, failure
+
+# Keyword lists and true values, also in log for time
+zenith_list =  {"20deg": 20*u.deg,
+                "40deg": 40*u.deg,
+                "60deg": 60*u.deg}
+
+dt_list    =  {"100s" : (100*u.s),
+               "30m"  : (30*u.min).to(u.s),
+               "05h"  : (5*u.h).to(u.s),
+               "50h"  : (50*u.h).to(u.s)}
+
+dtl    =  {"100s" : np.log10((100*u.s).value),
+           "30m"  : np.log10((30*u.min).to(u.s).value),
+           "05h"  : np.log10((5*u.h).to(u.s).value),
+           "50h"  : np.log10((50*u.h).to(u.s).value)
+                   }
+
+# Validity range of IRF in zenith (see documentation of this module).
+# The 60deg IRF is allowed to be used down to alitude zero for tests
+# Its use if ofreseen tobe limitee bythe altmin variable
+theta_valid = {"20deg": [0*u.deg, 33*u.deg],
+               "40deg": [33*u.deg, 54*u.deg],
+               "60deg": [54*u.deg, 90*u.deg] # Should be limited by altmin
+               }
+
+# Validity range of IRF in time, taking into account that the validity
+# intervals are somehow in log. scale
+# The edge values are the following "
+# 0, 424.26 s, 5692.01 s (94.9'), 56921.0 s (15.8h)
+
+dt_log_valid = {"100s": [0,
+                         10**(0.5*( dtl["100s"] + dtl["30m"] )) ],
+                "30m" : [10**(0.5*( dtl["100s"] + dtl["30m"] )),
+                         10**(0.5*( dtl["30m"]  + dtl["05h"] )) ],
+                "5h"  : [10**(0.5*( dtl["30m"]  + dtl["05h"] )),
+                         10**(0.5*( dtl["05h"]  + dtl["50h"] )) ],
+                "50h" : [10**(0.5*( dtl["05h"]  + dtl["50h"] )),
+                         np.Inf]
+                }
+
+# Minimal acceptable energies depending on the IRF
+# (resp. generated and reconstructed energies)
+egen_min = {"20deg": 12.6*u.GeV,
+            "40deg": 26.4*u.GeV,
+            "60deg": 105.3*u.GeV
+            }
+erec_min = {"20deg": 40*u.GeV,
+            "40deg": 26.4*u.GeV,
+            "60deg": 110*u.GeV
+            }
+
 
 __all__=['IRF']
-
 
 ###############################################################################
 class IRF(object):
     """
-    Class to handle time variable IRF
-    
-    Information from G. Maier obtained by I. Sadeh
-    Preamble : the latest prod versions are prod3v2
+        Class to handle time variable IRF.
 
-    Former IRf can be used they essentially differ in the angular resolution at 
-    high energies. Include both full array, threshold array and LST-only IRFs. 
-    See here :
-    https://forge.in2p3.fr/projects/cta_analysis-and-simulations/wiki/Prod3b_based_instrument_response_functions
-
-    
-    High NSB : 
-        The earlier prod3b productions can be used.
-        See here : https://forge.in2p3.fr/attachments/download/55554
-        They cover 20 and 40 degrees and not 60 degrees.
-        They can be used as an indicator for the systematics, 
-        not as a precise prediction of performance.
-        They have 5x or 30x the nominal NSB. 
-        High NSB IRFs are only available for 20 deg zenith angle. 
-
-    Interpolation : 
-        
-        ZENITH 
-        Gernot recommends to define the zenith  windows by 1/cos(theta). 
-        This means (Iftach says):
-            zen < 33        —> use IRF 20deg
-            33 <= zen < 54  —> use IRF 40deg
-            zen > 54        —> use IRF 60deg
-        
-        AZIMUTH
-        We should use the average azimuth (not the step function):
-        
-        ENERGY
-        We should generate events from the minimum allowed value for 
-        a given IRF, but then we should only use for the analysis events above 
-        a slightly higher threshold, in order to avoid cutoff effects. 
-        
-        The IRFs are defined with these minimum energy thresholds:
-            20deg —> E_gen >= 12.6 GeV
-            40deg —> E_gen >= 26.4 GeV
-            60deg —> E_gen >= 105.3 GeV
-        Iftach suggests (does not says if generated ot reconstrcuted):
-            20deg —> E_grb >= 30 GeV
-            40deg —> E_grb >= 40 GeV
-            60deg —> E_grb >= 110 GeV
-        That corresponds roughly to [E_gen + deltaE], which is the 
-        expected energy resolution at low energies (deltaE/E (68%) = 0.25)
-
-    Note also that the CTA performance plots have a cutoff of E>20 GeV 
-    since we can’t trust the sims below this range. 
-    In terms of requirements, we actually only have a guarantee above 30 GeV, 
-    again, due to the large uncertainties at these energies. 
-    -> We can therefore not use <30GeV energies for spectra/detection in any 
-    case for a consortium publication
-    
     """
 
     ###########################################################################
     def __init__(self,
-                 folder = cf.irf_dir,
+                 folder = "../input/irf/OnAxis/",
                  kchain = "Reco1",
                  khem   = "North",
                  array  = "FullArray",
                  kzen   = "20deg",
                  kaz    = "average",
                  kdt    = "100s",
-                 nsb=False):
+                 nsb    = False):
         """
-        tbw
+        Define default values for getting IRF data from an IRF file folder
+        and keywords.
 
         """
-
-        self.theta_list =  {"20deg":20*u.deg,"40deg":40*u.deg,"60deg":60*u.deg}
-        self.dt_list    = {"100s": (100*u.s),
-                           "30m" : (30*u.min).to(u.s),
-                           "05h" : (5*u.h).to(u.s),
-                           "50h" : (50*u.h).to(u.s)}
-
         self.folder = folder # path
         self.name   = "undef"# irf filename
         self.kchain = kchain # key
@@ -117,7 +172,11 @@ class IRF(object):
         return
 
     ###########################################################################
-    def __str__(self):
+    def print(self):
+        """
+        Print out the IRF class content
+
+        """
 
         print("========================================================")
         if (os.path.exists(self.folder)):
@@ -135,45 +194,93 @@ class IRF(object):
         print("  Duration       : ",self.kdt)
         print("  High NSB       : ",self.nsb)
 
-        return ""
+        return
 
     ###########################################################################
     @classmethod
-    def get_irf_file(cls, theta, phi, obstime, khem="North",**kwargs):
+    def get_irf_file(cls, zenith, azimuth, obstime, loc = None,
+                     closest = False):
+        """
+        From the current observation features, zenith and azimuth angles
+        and observation time, find the best IRF available among the available
+        list of samples angles and duration. Define the keys allowing to
+        retrieve the corresponding IRF file on disk.
 
-        # This calls the default constructor
-        # Don't forget to pass the location key otheriwise, North!
-        irf = cls(khem=khem,**kwargs)
+        Parameters
+        ----------
+        cls : IRF class
+            Current IRF instance
+        theta :
+            DESCRIPTION.
+        phi : TYPE
+            DESCRIPTION.
+        obstime : TYPE
+            DESCRIPTION.
 
-        # First find the best keys
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        """
+        self=cls()
+
+        if (loc == None):
+            sys.exit("Get_Irf_file : location should be defined")
+
+        ###--------------------------
         def find_best_key(mydict,x):
             dict_values    = np.asarray([x.value for k, x in mydict.items() ])
             closest_val = dict_values [  (np.abs(dict_values - x.value)).argmin() ]
             closest_key = [k for k, x in mydict.items() if x.value == closest_val]
             return closest_key[0]
+        ###--------------------------
 
-        irf.kzen   = find_best_key(irf.theta_list,theta)
-        irf.kaz    = "average"
-        irf.kdt    = find_best_key(irf.dt_list,obstime.to(u.s))
+        # Zenith
+        if (closest):
+            self.kzen = find_best_key(zenith_list, obstime)
+        else:
+            found = False
+            for k,v in theta_valid.items():
+                if (zenith >=v[0] and zenith < v[1] ):
+                    self.kzen = k
+                    found = True
+                    continue
+            if (not found):
+                sys.exit("get_irf_file: zenith= {} => range not found"
+                         .format(zenith))
 
-        # print(" Best theta : ",irf.kzen," -> ",irf.theta_list[irf.kzen])
-        # print(" Best phi = ",irf.kaz)
-        # print(" Best obs : ",irf.kdt," -> ",irf.dt_list[irf.kdt])
-        
-        return irf.get_file_fromkeys()
+        # Azimuth - implement here N, S choice
+        self.kaz = "average"
 
-###########################################################################
-    def get_file_fromkeys(self,missing=0):
+        # Observation time
+        if (closest):
+            self.kdt =find_best_key(dt_list, obstime.to(u.s))
+        else:
+            found = False
+            for k,v in dt_log_valid.items():
+                if obstime.to(u.s).value >= v[0] \
+                   and obstime.to(u.s).value < v[1] :
+                    self.kdt = k
+                    found = True
+                    continue
+            if (not found):
+                sys.exit("get_irf_file: obstime range not found")
+
+        return self.get_file_fromkeys()
+
+    ###########################################################################
+    def get_file_fromkeys(self,fixed_zenith = None, missing=0):
         """
         Find folder name and filename of an IRf file based on a series of
         keywords. In case no corresponding file is found, go back to
         a default set of keywords.
-        This has to be tuned with the existing list of available IRF
+        This has to be tuned with the existing list of available IRF.
         """
 
-        if (cf.fixed_zenith != False):
-            self.kzen = cf.fixed_zenith
-            
+        if (fixed_zenith != None):
+            self.kzen = fixed_zenith
+
         folder = Path(self.folder,self.kchain,self.khem,self.array,self.kzen)
         if folder.exists() != True:
             self.khem = "North"
@@ -183,9 +290,9 @@ class IRF(object):
             if folder.exists() != True:
                 failure(" Impossible to find a suitable IRF folder")
                 sys.exit("Stopped")
-            
+
         irf_file = []
-        
+
         for file in os.listdir(folder):
             if (file.find(self.khem) != -1) and \
                (file.find(self.kzen) != -1) and \
@@ -266,30 +373,34 @@ class IRF(object):
 
 ###############################################################################
 if __name__ == "__main__":
+    """
+    Code example to use the IRF class
+    """
 
-    # Create an IRF object - get the default values
-#    myirf = IRF(folder=cf.irf_dir,array="LST")
-#    myirf.print()
-#    irf_file = myirf.get_file_fromkeys()
-#    print(irf_file)
-#
-#    # Create an IRF object - get a  values
-#    myirf = IRF(kzen="40deg")
-#    myirf.print()
-#    irf_file = myirf.get_file_fromkeys()
-#    print(irf_file)
+    import ana_config as cf
 
     # Get file form specific values
     print("=====================================================")
     irf = IRF()
-    irffile = irf.get_irf_file(khem="South",
-                               theta=55*u.deg, 
-                               phi=123*u.deg,
-                               obstime=23*u.min)
+    irffile = irf.get_irf_file(loc="South",
+                               zenith  =55*u.deg,
+                               azimuth =123*u.deg,
+                               obstime =95*u.h)
+    irf.print()
     print(irffile)
-    print(irf)
+    print(dt_log_valid)
+    # Create an IRF object - get the default values and the corresponding file
+    case1 = False
+    if (case1):
+        myirf = IRF(folder=cf.irf_dir,array="LST")
+        myirf.print()
+        irf_file = myirf.get_file_fromkeys()
+        print(irf_file)
 
-#    myirf = IRF(khem="North", kaz="average",kzen="20deg",kdt="100s")
-#    myirf.print()
-#    irf_file = myirf.get_file_fromkeys()
-#    print(irf_file)
+    case2 = False
+    if (case2):
+        # Create an IRF object - get a  values
+        myirf = IRF(kzen="40deg")
+        myirf.print()
+        irf_file = myirf.get_file_fromkeys()
+        print(irf_file)
