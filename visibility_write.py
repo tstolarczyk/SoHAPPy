160e7c974141db394f56a2d5035d9cc74ef75d6b
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2021-08-19 17:19:13 +0200

    Update visibility_write.py

--- visibility_write.py
+++ visibility_write.py
@@ -14,24 +14,24 @@ os.environ['GAMMAPY_DATA'] =r'../input/gammapy-extra-master/datasets'
 
 from SoHAPPy import get_grb_fromfile
 import grb_plot as gplt
-
-#import ana_config as cf # Steering parameters
 import time
 
 
-# Overseed GRB parameters
-altmin     = 24*u.deg
-altmoon    = 90*u.deg
-moondist   = 0*u.deg  # Moon minimal distance
-moonlight  = 1        # Moon maximum brigthness
-depth      = 3*u.day # Maximum duration after trigger to compute the visibility.
-skip       = 0
-vis_folder = "."
+depth     = 3        # (3) Maximum number of nights to compute the visibility.
+skip      = 0        # (0) Number of first nights to be skipped
+
+# Suggested best default affording for moon light
+altmin    =  24*u.deg   # Minimum altitude (original default is 10 degrees)
+altmoon   = -0.25*u.deg # Moon maximum altitude (-0.25 u.deg for horizon)
+moondist  =  30*u.deg   # Moon minimal distance
+moonlight =  0.6        # Moon maximum brigthness
+
+vis_folder = "./vis_24_moonlight"
 
 dbg  = 0
 show = 0
 
-ngrb    = 2 # 250
+ngrb    = 1000 # 250
 ifirst  = 1
 
 readvis = False # If True, recovery testing
@@ -58,11 +58,13 @@ for i in grblist:
     grb = get_grb_fromfile(i,log=log)
 
     for loc in ["North","South"]:
-        grb.vis[loc].compute(altmin  = altmin,
-                                  altmoon = altmoon,
-                                  depth   = depth,
-                                  skip    = skip,
-                                  debug=False)
+        grb.vis[loc].compute(altmin    = altmin,
+                             altmoon   = altmoon,
+                             moondist  = moondist,
+                             moonlight = moonlight,
+                             depth     = depth,
+                             skip      = skip,
+                             debug=False)
     if dbg > 0:
         print(grb)
         grb.vis["North"].print(log=log)
@@ -81,7 +83,7 @@ for i in grblist:
 stop = time.time() # Starts chronometer
 log.prt("Completed in {:8.2f} s ({:4.2f} s per source)".format(stop-start,(stop-start)/ngrb))
 
-read_vis = True
+read_vis = False
 if (read_vis):
     from visibility import Visibility
     print("\n .................... RECOVERING ...............\n")
