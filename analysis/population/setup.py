0529510297dc90d10503571d4242d6622da1073a
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2021-10-04 13:49:28 +0200

    Create setup.py
    global variables for the analysis

---
+++ analysis/population/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+col_n = "tab:blue"
+col_s = "tab:red"
+col_b = "tab:purple"
+col_3 = "tab:orange"
+col_5 = "tab:green"
+dtpop = 44 # Populatin equivalent tie in years
+
+import sys
+codefolder = "../../" 
+sys.path.append(codefolder) 
+
+
