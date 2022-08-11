f74e92a94555b8802e077859af0fdc0b8deab4ce
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2022-08-11 18:25:06 +0200

    Add function to read data from a tar file
    Used to get the txt file directly from the archive.

--- utilities.py
+++ utilities.py
@@ -9,9 +9,87 @@ import numpy as np
 import astropy.units as u
 
 ###----------------------------------------------------------------------------
-# A label for plots and histograms with statistics
+def file_from_tar(folder=None, tarname=None, target=None):
+    """
+    
+
+    Parameters
+    ----------
+    folder : string, optional
+        The folder to scan. The default is "None".
+    tarname : string, optional
+        The archive name in case more than one in the folder. 
+        The default is None.
+    target : string, optional
+        The file to be found in the archive, or a file name with the same 
+        extension. The default is None.
+
+    Returns
+    -------
+    A pointer to a file in memory
+
+    """    
+    
+    import tarfile
+    from pathlib import Path
+    
+    tag = "file_from_tar: "
+    
+    ### ------------------------
+    ### Get archive file name
+    ### ------------------------
+    if folder == None: sys.exit("{} Folder not found".format(tag))
+    
+    if target == None: 
+        sys.exit("{} Specify a target in the archive".format(tag))
+    
+    if tarname == None:
+        # Find the archive inthe folder
+        p = Path(folder).glob('*.tar.gz')
+        files = [x for x in p if x.is_file()]
+        if len(files)>1:
+            sys.exit("{} More than one .tar.gz file, specify a name"
+                     .format(tag))
+        else:
+            tarname = files[0]
+    print("{} found {}".format(tag,tarname))    
+
+    ### ------------------------
+    ### Open tar file, get members, check data.txt exists
+    ### ------------------------
+    tar = tarfile.open(tarname, "r:gz")
+        
+    # If the target is not found explicitely, tires a file with same extension
+    if not target in [member.name for member in tar.getmembers()]:
+        
+        print("{} No {} in archive. Tries with extension"
+              .format(tag, target))
+        
+        # find members with correct extension
+        extmatch = \
+            [(m.name if Path(m.name).suffix==Path(target).suffix else None) \
+             for m in tar.getmembers()]
+        extmatch = list(filter(None,extmatch)) # Remove None
+    
+        if len(extmatch)>1: 
+            sys.exit("{} More than one file matching in archive (try largest?)"
+                     .format(tag))
+        elif len(extmatch)==0: 
+            sys.exit("{} No file matching extension in the archive"
+                     .format(tag))
+        else: 
+            print("{} {} matches {}".format(tag, extmatch[0],target))
+            target = extmatch[0]
+    
+    # At this stage, ether the target was found or deduced from the extension
+    print("{} A file matching {} was found".format(tag,target))
+    datafile = tar.extractfile(target)       
+            
+    return datafile
+###----------------------------------------------------------------------------
 def MyLabel(var,label="",stat="std"):
     """
+    A label for plots and histograms with statistics.
     Add extra statistical information (counts, dispersions) to a classical 
     matplotlib label. 
 
