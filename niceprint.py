ca9d7b8f791f75ceccba0c7c8e320af5d1e27daa
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2023-01-13 16:31:00 +0100

    Update niceprint.py
    Corrections from the spyder analysis tool + heading function

--- niceprint.py
+++ niceprint.py
@@ -22,7 +22,7 @@ def t_str(t, digit=2):
         The formatted and rounded time as a string.
 
     """
-    
+
     t = t_fmt(t)
     return str( round(t.value,digit)) +" "+ str(t.unit)
 #------------------------------------------------------------------------------
@@ -42,7 +42,7 @@ def t_fmt(t, digit=None):
 
     """
     import astropy.units as u
-    
+
     # Get and format livetimes
     t = t.to(u.s)
     if   t.value > 1.5*3600*24: t = t.to(u.d)
@@ -53,29 +53,35 @@ def t_fmt(t, digit=None):
     else:             return t
 
 ###----------------------------------------------------------------------------
+def heading(title):
+    print(f"+{78*'-':78s}+")
+    print(f"+{title:^78s}+")
+    print(f"+{78*'-':78s}+\n")
+
+###----------------------------------------------------------------------------
 def textcol(text,t="black",b="white",s=None):
     """
     Change text color.
     See:
     https://www.instructables.com/id/Printing-Colored-Text-in-Python-Without-Any-Module/
     Add color to text in python : https://ozzmaker.com/add-colour-to-text-in-python/
-    
-    To make some of your text more readable, you can use ANSI escape codes to 
-    change the colour of the text output in your python program. A good use 
+
+    To make some of your text more readable, you can use ANSI escape codes to
+    change the colour of the text output in your python program. A good use
     case for this is to highlight errors.
     The escape codes are entered right into the print statement.
-    
+
     print("\033[1;32;40m Bright Green \n")
-    
-    The above ANSI escape code will set the text colour to bright green. 
-    The format is; \033[ Escape code, this is always the same 1 = Style, 
-    1 for normal. 32 = Text colour, 32 for bright green. 
+
+    The above ANSI escape code will set the text colour to bright green.
+    The format is; \033[ Escape code, this is always the same 1 = Style,
+    1 for normal. 32 = Text colour, 32 for bright green.
     40m = Background colour, 40 is for black.
-    
-    This table shows some of the available formats; 
-    TEXT COLOR CODE TEXT STYLE CODE BACKGROUND COLOR CODE 
-    Black 30 No effect 0 Black 40 Red 31 Bold 1 Red 41 Green 32 Underline 
-    2 Green 42 Yellow 33 Negative1 3 Yellow 43 Blue 34 Negative2 5 Blue 
+
+    This table shows some of the available formats;
+    TEXT COLOR CODE TEXT STYLE CODE BACKGROUND COLOR CODE
+    Black 30 No effect 0 Black 40 Red 31 Bold 1 Red 41 Green 32 Underline
+    2 Green 42 Yellow 33 Negative1 3 Yellow 43 Blue 34 Negative2 5 Blue
     44 Purple 35 Purple 45 Cyan 36 Cyan 46 White 37 White 47
 
     Parameters
@@ -116,7 +122,7 @@ def textcol(text,t="black",b="white",s=None):
     if (b != None): code = code + "4"+color[b] + ";"
     code = code[:-1] + "m"
     endcode = "\033[m"
-    
+
     return code+text+endcode
 
 ###----------------------------------------------------------------------------
@@ -132,30 +138,30 @@ def success(text,**kwarg):
 def highlight(text,**kwarg):
     print(textcol(text,s="bold"),**kwarg)
 def banner(text,**kwarg):
-    print(textcol(text,t="black",b="yellow",s="bold"),**kwarg)    
+    print(textcol(text,t="black",b="yellow",s="bold"),**kwarg)
     return
-    
+
 ###----------------------------------------------------------------------------
 class Log():
     """
     A class to manage a logbook information
-    
+
     """
     import sys
     ###------------------------------------------------------------------------
     def __init__(self, name="default.log",talk=True):
-        
+
         from pathlib import Path
         name = Path(name) # In case this would not be a path
-        
+
         if not name.absolute().parent.exists(): # Check folder exists
             name.absolute().parent.mkdir(parents=True, exist_ok=True)
-     
-        try:                                                                                                                                                  
+
+        try:
             self.log_file = open(name,'w')
         except IOError:
             print("Failure opening {}: locked".format(name))
-        
+
         self.name     = name
         self.talk     = talk
         return
@@ -208,7 +214,7 @@ class Log():
             print(textcol(text,t="black",b="yellow",s="bold"),**kwarg)
         if (self.log_file != None):
             print(" **** " + text + " *** ",**kwarg,file=self.log_file)
-            
+
 ###----------------------------------------------------------------------------
 if __name__ == "__main__":
     log = Log()
