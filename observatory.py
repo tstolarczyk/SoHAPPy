39795e0a2ae0b529ff477029d7ffe9a282e7af08
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2023-08-08 18:01:22 +0200

    Update observatory.py
    code analysed

--- observatory.py
+++ observatory.py
@@ -2,12 +2,17 @@
 """
 Created on Fri Sep 16 10:26:34 2022
 
+This module contains information on various observatory locations.
+
 @author: Stolar
 """
 import astropy.units as u
 from   astropy.coordinates   import Angle, EarthLocation
+
+__all__ = ["xyz"]
+
 ### Site positions
-xyz = { 
+xyz = {
     "CTA":
          { "North": EarthLocation.from_geocentric( float(5327448.9957829),
                                                    float(-1718665.73869569),
@@ -29,14 +34,14 @@ xyz = {
            # "South": EarthLocation.from_geocentric(1946635.7979987,
            #                                       -5467633.94561753,
            #                                       -2642498.5212285,
-           #                                        unit="m")   
+           #                                        unit="m")
            # Alternative :
            # "North": EarthLocation.from_geodetic('342.1184',
            #                                       '28.7606',2326.* u.meter)
            # "South": EarthLocation.from_geodetic('289.5972',
            #                                      '-24.6253',2635.* u.meter)
          },
-         
+
   # from default values in
   # https://www.mpi-hd.mpg.de/hfm/HESS/pages/home/visibility/
   # Warning : use CTA-North for virtual HESS North
