1b2ea8c0875d85696ebe40d2955b471d398d36f6
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2023-08-08 18:02:38 +0200

    Update niceplot.py
    code analysed

--- niceplot.py
+++ niceplot.py
@@ -2,16 +2,43 @@
 """
 Created on Mon Oct 17 09:56:58 2022
 
+A bunch of functions to manipulate objects related to plots.
+
 @author: Stolar
 """
-import numpy as np
-import matplotlib.pyplot as plt
 
-from matplotlib.colors import LightSource
-from mpl_toolkits import mplot3d
+from collections import OrderedDict
+import gammapy
+
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from matplotlib import cm
+
+__all__ = ["pause", "MyLabel","single_legend","vals_legend","stamp",
+           "projected_scatter","ColorMap","col_size","draw_sphere"]
+###----------------------------------------------------------------------------
+def pause():
+    """
+    Used to pause plot display in interactive mode on a shell script. In the
+    abscence of a call to that function figures wil stack on the screen during
+    the run and all disappear at the end of the run.
+    Using this, figures will be stacked on screen and displayed for each event
+    until they are closed by the user.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    plt.show(block=True)
 
 ###----------------------------------------------------------------------------
-def MyLabel(var,label="",stat="std"):
+def MyLabel(var, label="", stat="std"):
+
     """
     A label for plots and histograms with statistics.
     Add extra statistical information (counts, dispersions) to a classical
@@ -35,14 +62,16 @@ def MyLabel(var,label="",stat="std"):
 
     """
 
-    if len(label)!=0 : label = label+"\n"
+    if len(label) != 0 :
+        label = label+"\n"
 
     legend="bad option"
-    if stat == None: return label + "$n$ : {:d}".format(len(var))
+    if stat is None:
+        return label + f"$n$ : {len(var):d}"
 
-    if stat.find("std") !=-1:
+    if stat.find("std") != -1:
         legend = label  \
-                + "$n$ : {:d} \n".format(len(var)) \
+                + f"$n$ : {len(var):d} \n".format(len(var)) \
                 + r"$\bar{n}$ : "+"{:5.3f}\n".format(np.mean(var)) \
                 + r"$\sigma$ : "+" {:5.3f}".format(np.std(var))
     elif stat.find("med") !=-1 :
@@ -54,10 +83,11 @@ def MyLabel(var,label="",stat="std"):
     return legend
 
 ###----------------------------------------------------------------------------
-def single_legend(ax,**kwargs):
+def single_legend(ax, **kwargs):
+
     """
     Remove duplicated labels in legend (e.g. a vertical and an horizontal
-    lines defining and intersection havingthe same labels).
+    lines defining and intersection having the same labels).
 
     Parameters
     ----------
@@ -69,22 +99,19 @@ def single_legend(ax,**kwargs):
     None.
 
     """
-    from collections import OrderedDict
+
     handles, labels = ax.get_legend_handles_labels()
     by_label = OrderedDict(zip(labels, handles))
     ax.legend(by_label.values(), by_label.keys(),**kwargs)
 
-    return
-
 ###----------------------------------------------------------------------------
-def vals_legend(ax, vals=[5 , 10, 20, 50, 100, 500], alpha=0.5, var_max=1000, **kwargs):
+def vals_legend(vals = None, alpha = 0.5, var_max = 1000, **kwargs):
+
     """
-    Create a legend with colored circles form a list of values    
+    Create Matplotlib patches with colored circles from a list of values.
 
     Parameters
     ----------
-    ax : Matplotlib axes
-        Current axis.
     vals: list of float
         List of values in legend
     alpha : float, optional
@@ -101,51 +128,63 @@ def vals_legend(ax, vals=[5 , 10, 20, 50, 100, 500], alpha=0.5, var_max=1000, **
 
     """
 
+    print("niceplot.vals_legend: Investigate why ax is not used")
+    if vals is None:
+        vals = [5 , 10, 20, 50, 100, 500]
+
     labels = [str(x) for x in vals]
-    # symbol = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="red")
+    # symbol = Line2D(range(1), range(1), color="white", marker='o',
+    # markerfacecolor="red")
 
     colors, sizes = col_size(vals,var_max=var_max)
     patches = [ plt.plot([],[], marker="o", alpha=alpha,
                           ms = 7+sizes[i]/50,
                           ls="", mec=None,
                           color=colors[i],
-                          label="{:s}".format(labels[i]) )[0]  for i in range(len(labels)) ]
-    # ax.legend(title="$\sigma_{max}$",handles=patches,**kwargs)
+                          label="{:s}".format(labels[i]) )[0]  for i in range(len(labels),
+                          **kwargs) ]
 
     return patches
+
 ###----------------------------------------------------------------------------
-def stamp(text, axis=None,
-          where="right",x=None, y=None, rotation=0, **kwargs):
+def stamp(text, axis = None,
+          where = "right", x = None, y = None, rotation = 0,
+          **kwargs):
     """
     Annotation on the side of any plot referred from the axis, including
     the gammapy version.
 
+
     Parameters
     ----------
     text : String
-        Text to be displayed
+       Text to be displayed
     where : String, optional
-        Position of the text with respect to the axis. The default is "right".
+       Position of the text with respect to the axis.
+       The default is `right`.
     x : float, optional
-        Text x position, supersedes the where variable. The default is None.
+       Text x position, supersedes the `where` variable.
+       The default is `None`.
     y : float, optional
-        Text x position, supersedes the where variable. The default is None.
+       Text x position, supersedes the `where` variable.
+       The default is `None`.
     rotation : float, optional
-        Text rotation - If not given use default. The default is None.
-     axis : matplotlib.axes, optional
-         Current plot axis. The default is None.
+       Text rotation - If not given use default.
+       The default is `None`.
+    axis : matplotlib.axes, optional
+       Current plot axis. The default is `None`.
     **kwargs :
-        Any additionnal arguments for matplotlib.axes.text
+       Any additionnal arguments for matplotlib.axes.text
 
     Returns
     -------
     None.
+
     """
 
-    import gammapy
     text = text + " - " + gammapy.__version__
 
-    if x==None or y== None:
+    if x is None or y is None:
         if   where =="right":
             (x,y) = (  1, 0.5)
             rotation = 270
@@ -162,12 +201,13 @@ def stamp(text, axis=None,
     axis.text(x=x,y=y,s=text,
               horizontalalignment='center',
               verticalalignment="center",
-              rotation=rotation)
-    return
+              rotation=rotation, **kwargs)
+
 ###----------------------------------------------------------------------------
-def projected_scatter(xsize=12, ysize=8,
-                      left=0.1, width=0.7, bottom=0.1, height=0.7,
-                      spacing=0.02):
+def projected_scatter(xsize   = 12,  ysize  = 8,
+                      left    = 0.1, width  = 0.7,
+                      bottom  = 0.1, height = 0.7,
+                      spacing = 0.02):
     """
     Matplotlib template to display a scatter plot and the horizontal and
     vertical projections of the data. Adapted from :
@@ -202,8 +242,6 @@ def projected_scatter(xsize=12, ysize=8,
         Vertical projection axis.
 
     """
-    import matplotlib
-
     rect_scatter = [left, bottom, width, height]
     rect_histx   = [left, bottom + height + spacing, width, 0.2]
     rect_histy   = [left + width + spacing, bottom, 0.2, height]
@@ -236,10 +274,6 @@ def ColorMap(threshold,maxval):
         A matplotlib color map.
 
     """
-
-    import matplotlib.pyplot as plt
-    import matplotlib.colors as mcolors
-
     color_cut = int(256*threshold/maxval)
     print("color_cut=",color_cut)
     colors1  = plt.cm.Greys(np.linspace(0, 1, color_cut))
@@ -253,7 +287,27 @@ def ColorMap(threshold,maxval):
 ###----------------------------------------------------------------------------
 def col_size(var,var_min=1.1,var_max = 1000):
 
-    import matplotlib.cm as cm
+    """
+    get a color and a size from a numerical value
+
+    Parameters
+    ----------
+    var : float
+        Input number.
+    var_min : float, optional
+        Minimal value. The default is 1.1.
+    var_max : float, optional
+        Maximal value. The default is 1000.
+
+    Returns
+    -------
+    color : matplotlib color
+        Color.
+    size : float
+        matplotlib size.
+
+    """
+
     # Limit values in case they are not yet limited
     var   = np.clip(var, var_min, None)
 
@@ -263,13 +317,31 @@ def col_size(var,var_min=1.1,var_max = 1000):
     return color, size
 
 ###----------------------------------------------------------------------------
-def draw_sphere(radius=1, colormap=plt.cm.viridis,ax=None, **kwargs):
+def draw_sphere(radius=1, colormap=plt.cm.viridis, ax=None, **kwargs):
+    """
+    Draw a sphere in matplotlib.
 
-    if ax==None:
+    Parameters
+    ----------
+    radius : float, optional
+        radius. The default is 1.
+    colormap : matplolib color map, optional
+        Color map. The default is plt.cm.viridis.
+    ax : matplolib axes, optional
+        Current axis. The default is None.
+    **kwargs :
+        Extra arguments.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    if ax is None:
         fig = plt.figure(figsize=(8,8), dpi=300)
         ax = fig.add_subplot(111, projection='3d')
-    
-    import matplotlib
+
     if matplotlib.__version__ > "3.5":
         ax.set_box_aspect(aspect = (1,1,1))
 
@@ -279,12 +351,10 @@ def draw_sphere(radius=1, colormap=plt.cm.viridis,ax=None, **kwargs):
     x = radius* np.outer(np.cos(u), np.sin(v))
     y = radius* np.outer(np.sin(u), np.sin(v))
     z = radius* np.outer(np.ones(np.size(u)), np.cos(v))
-    
-    ls = LightSource(azdeg=0, altdeg=65)
+
+    ls = mcolors.LightSource(azdeg=0, altdeg=65)
     rgb = ls.shade(z, colormap)
 
-    ax.plot_surface(x, y, z,  rstride=1, cstride=1, 
-#                     color=color,   
+    ax.plot_surface(x, y, z,  rstride=1, cstride=1,
+#                     color=color,
                     facecolors=rgb, linewidth=0, **kwargs)
-
-    return
