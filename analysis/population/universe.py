9549e82bfa2b418c320b3eb45879195192e4ae17
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2025-11-25 15:02:24 +0100

    Update universe.py

--- analysis/population/universe.py
+++ analysis/population/universe.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan  4 17:22:01 2023
+Created on Wed Jan  4 17:22:01 2023.
 
 Show universe coverage.
 
@@ -9,15 +9,14 @@ Show universe coverage.
 
 import sys
 
-import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 import astropy.coordinates as coord
 import astropy.units as u
-from astropy.cosmology import Planck13 as cosmo # astropy v5
+from astropy.cosmology import Planck13 as cosmo  # astropy v5
 # from astropy.coordinates.distances import Distance
-from   astropy.visualization import quantity_support
+from astropy.visualization import quantity_support
 
 from niceplot import col_size, vals_legend, draw_sphere
 
@@ -25,16 +24,18 @@ from population import Pop
 from pop_io import get_data
 
 # Bigger texts and labels
-sns.set_context("notebook") # poster, talk, notebook, paper
+sns.set_context("notebook")  # poster, talk, notebook, paper
 
 codefolder = "../../"
 sys.path.append(codefolder)
 
 __all__ = ["universe_coverage"]
-###----------------------------------------------------------------------------
+
+
+# ##---------------------------------------------------------------------------
 def universe_coverage(sub_pop):
     """
-
+    Blabla.
 
     Parameters
     ----------
@@ -46,18 +47,11 @@ def universe_coverage(sub_pop):
     None.
 
     """
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection='3d')
+    ax.set_box_aspect(aspect=(1, 1, 1))
 
-
-    # if matplotlib.__version__ < "3.5":
-    #     print(" Does not work with matplotlib ",matplotlib.__version__)
-    #     return
-
-    fig = plt.figure(figsize=(10,10))
-    ax=fig.add_subplot(projection='3d')
-    if matplotlib.__version__ >= "3.5":
-        ax.set_box_aspect(aspect = (1,1,1))
-
-    colors, sizes= col_size(sub_pop.sigmx)
+    colors, sizes = col_size(sub_pop.sigmx)
 
     distance = cosmo.luminosity_distance(sub_pop.z).to('Gpc')
     c1 = coord.SkyCoord(ra=sub_pop.ra*u.deg, dec=sub_pop.dec*u.deg,
@@ -67,13 +61,13 @@ def universe_coverage(sub_pop):
     x = gc1.x.to('Gpc')
     y = gc1.y.to('Gpc')
     z = gc1.z.to('Gpc')
-    dmax = max(max(x),max(y),max(z))
+    dmax = max(max(x), max(y), max(z))
     print(dmax)
 
     with quantity_support():
 
         ax.scatter(gc1.x, gc1.y, gc1.z, zdir='z',
-                   marker="o",s=sizes,color=colors)
+                   marker="o", s=sizes, color=colors)
         ax.set_xlabel(None)
         ax.set_ylabel(None)
         ax.set_zlabel(None)
@@ -81,21 +75,25 @@ def universe_coverage(sub_pop):
         ax.set_ylim(ymin=-dmax, ymax=dmax)
         ax.set_zlim(zmin=-dmax, zmax=dmax)
 
-    draw_sphere(radius=dmax.value,ax=ax,alpha=0.1)
-    draw_sphere(radius=cosmo.luminosity_distance(2.5).to('Gpc').value,ax=ax,alpha=0.1)
+    draw_sphere(radius=dmax.value, ax=ax, alpha=0.1)
+    draw_sphere(radius=cosmo.luminosity_distance(2.5).to('Gpc').value,
+                ax=ax, alpha=0.1)
 
     patches = vals_legend()
-    fig.legend(title=r"$\sigma_{max}$",handles=patches,bbox_to_anchor=[1.15, 0.800],ncol=1)
+    fig.legend(title=r"$\sigma_{max}$",
+               handles=patches, bbox_to_anchor=[1.15, 0.800],
+               ncol=1)
 
     plt.tight_layout()
 
-################################################################################################
+
+###############################################################################
 if __name__ == "__main__":
 
-    nyears, files, tag = get_data(parpath=None,debug=True)
+    nyears, files, tag = get_data(parpath=None, debug=True)
     # nyears, files, tag = get_data(parpath="parameter.yaml",debug=False)
 
-    pop = Pop(files, tag=tag, nyrs= nyears)
+    pop = Pop(files, tag=tag, nyrs=nyears)
 
     # draw_sphere(radius=2.3)
-    universe_coverage(pop.g_tot[pop.g_tot.d5s>pop.eff_lvl])
+    universe_coverage(pop.g_tot[pop.g_tot.d5s > pop.eff_lvl])
