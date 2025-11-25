ee244a501fffdb6dc563554596a80b69d5b3a2ff
STOLARCZYK Thierry <Stolar@extra.cea.fr>
2025-11-25 15:03:27 +0100

    Update explore_simulation_from_fits.py

--- analysis/population/explore_simulation_from_fits.py
+++ analysis/population/explore_simulation_from_fits.py
@@ -1,205 +1,244 @@
 # -*- coding: utf-8 -*-
 """
-Read a set of Fits files to access the source information in the form of a
-GammaRayBusrt SoHAPPy class. As this is time consuming, the classes can be
-stored on disk for further use.
-
-Created on Mon Jan  9 14:33:09 2023
+Created on Tue Feb 11 17:12:17 2025.
 
 @author: Stolar
 """
 import os
-
-
-import pickle
+import sys
 import numpy as np
+import pickle
 from pathlib import Path
+
 import matplotlib.pyplot as plt
+
 import astropy.units as u
+from astropy.io import fits
+from astropy.table import Table, QTable
 from astropy.visualization import quantity_support
 
-from grb import GammaRayBurst
-import configuration
-from niceplot import MyLabel, single_legend
+from niceplot import MyLabel
+from configuration import Configuration
 
-__all__ = ["get_population","check_binning","flux_peak_time",
-           "flux_peak_time_all","check_spectra"]
-###----------------------------------------------------------------------------
-def get_population(ifirst=1, ilast=10, folder="."):
-    """
-    Create a list of GammaRayBurst classes from the input fits file
 
-    Parameters
-    ----------
-    ifirst : integer, optional
-        first source number. The default is 1.
-    ilast : integer, optional
-        Last source folde. The default is 10.
-    folder : string, optional
-        Source input folder name. The default is ".".
+class Fluxes():
+    """Blabla."""
 
-    Returns
-    -------
-    grbpop : list
-        List pf GammaRayBurst instances.
+    # ##-----------------------------------------------------------------------
+    def __init__(self):
+        """Initialize."""
+        self.Eval = None
+        self.tval = None
+        self.fluxes = None
+        self.Fpeaks = None
 
-    """
+    # ##-----------------------------------------------------------------------
+    @classmethod
+    def from_fits(cls, filelist):
+        """
+        Get the times.
 
-    cf = configuration.Configuration()
-    cf.ifirst     = ifirst
-    cf.nsrc       = ilast
-    grblist    = cf.source_ids()
+        Parameters
+        ----------
+        filelist : TYPE
+            DESCRIPTION.
 
-    grbpop=[]
-    for i, item in enumerate(grblist):
-        if ((cf.nsrc <= 10) or (np.mod(i,40) == 0)):
-            print("#",i+1," ",end="")
-        fname = Path(folder,"Event"+str(item)+".fits.gz" )
-        grb = GammaRayBurst.from_fits(fname, ebl     = "dominguez")
-        grbpop.append(grb)
-    grbpop = np.array(grbpop)
-    print("Population is created")
-    return grbpop
+        Returns
+        -------
+        tval : TYPE
+            DESCRIPTION.
+        Eval : TYPE
+            DESCRIPTION.
+        fluxval : TYPE
+            DESCRIPTION.
+        """
+        cls = Fluxes()
 
-###----------------------------------------------------------------------------
-def check_binning(grbpop):
-    nEval = []
-    ntval = []
-    for g in grbpop:
-        nEval.append(len(g.Eval))
-        ntval.append(len(g.tval))
-    fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
-    ax1.hist(nEval,label=MyLabel(nEval))
-    ax1.set_xlabel("Energy array length")
-    ax1.legend()
-    ax2.hist(ntval,label=MyLabel(ntval))
-    ax2.set_xlabel("Time array length")
-    ax2.legend()
-    plt.show()
+        ngrb = len(filelist)
+        first = True
 
-###----------------------------------------------------------------------------
-def flux_peak_time(grb,Eref=100*u.GeV):
+        fluxes = []
+        fpeaks = []
 
-    idxref  = np.abs(grb.Eval-Eref).argmin() # Get flux close to Eref
-    fluxref = grb.fluxval[:,idxref] # Obtain time spectrum for that energy
-    idmax   = fluxref.argmax() # Get time index of max value
+        for item, fname in enumerate(filelist):
 
-    return idmax, idxref
+            # Read file, header and keys
+            hdul = fits.open(fname)
+            hdr = hdul[0].header
+            keys_0 = list(hdul[0].header.keys())
 
-###----------------------------------------------------------------------------
-def flux_peak_time_all(energies, nbin=25):
-    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=len(energies),
-                                   figsize=(5*len(energies),10),
-                                   sharey=True)
+            # It can be long , give news from time to time
+            if first is True:
+                print("Processing items :", end=" ")
+            if np.mod(item, int(ngrb/50)) == 0 or ngrb < 10:
+                print(item, end=' ')
 
-    for Eref, ax01, ax02 in zip(energies, ax1, ax2):
-        tpeak = []
-        Fpeak = []
-        for grb in grbpop:
-            t_id, E_id = flux_peak_time(grb,Eref=Eref)
-            tmax = grb.tval[t_id]
-            Emax = grb.Eval[E_id]
-            flux = grb.fluxval[t_id][E_id]
-            # print(f" Max flux = {flux:8.2e} at t={tmax:8.2f} (E={Emax:5.2f})")
-            tpeak.append(tmax.value)
-            Fpeak.append(flux.value)
-        Fpeak = np.array(Fpeak)*flux.unit
-        tpeak = np.array(tpeak)*tmax.unit
+            # Get the time and energy bins from the first file only
+            # It assumes that all data files have the same binning
+            if first:
 
-        with quantity_support():
-            ax01.hist(tpeak, bins=nbin,label=MyLabel(tpeak))
-            ax01.set_title(f"{Emax.value:5.2f} {Emax.unit:s}")
-            ax01.set_xlabel("$t_{peak}$ (" + ax01.get_xlabel()+")")
-            ax01.legend()
+                tval = QTable.read(hdul["TIMES (AFTERGLOW)"])
+                cls.tval = np.array(tval[tval.colnames[0]].value)\
+                    * tval[0][0].unit
 
-            ax02.hist(Fpeak,bins=nbin)
-            ax02.set_xlabel("$F_{peak}$ (" + ax02.get_xlabel()+")")
+                tab_key = "Energies (afterglow)"
+                col_key = Table.read(hdul[tab_key]).colnames[0]
+                Eval = Table.read(hdul[tab_key])[col_key].quantity
+                cls.Eval = np.array(Eval)*Eval[0].unit
 
-        plt.tight_layout()
+                first = False
 
-###----------------------------------------------------------------------------
-def check_spectra(ifirst=0, ilast=3, Eref=100*u.GeV):
+            # Peak flux - used for seclecting the data
+            if "PHFLUX" in keys_0:
+                Fpeak = hdr['PHFLUX']*u.Unit("cm-2.s-1")
+            elif "PHFX" in keys_0:
+                Fpeak = hdr['PHFX']*u.Unit("cm-2.s-1")
+            fpeaks.append(Fpeak.value)
 
-    ngrb = ilast - ifirst
-    t_unit = u.d
-    fig, axall = plt.subplots(nrows=ngrb,ncols=1,figsize=(15,3.5*ngrb),
-                              sharex=True)
-    for grb, ax in zip(grbpop[ifirst:ilast+1],axall):
+            # Afterglow flux
+            flux = QTable.read(hdul["SPECTRA (AFTERGLOW)"])
+            flux_unit = u.Unit(flux.meta["UNITS"])
+            if str(flux_unit).find("ph") > -1:
+                flux_unit = flux_unit/u.Unit("ph")  # Removes ph
 
-        times = grb.tval[1:].to(t_unit)
-        dts   = (grb.tval[1:]-grb.tval[:-1]).to(t_unit)
+            # Store the flux. Note the transposition
+            itmax = len(tval) - 1
+            jEmax = len(Eval) - 1
+            fluxval = np.zeros((itmax+1, jEmax+1))
 
-        idxref  = np.abs(grb.Eval-Eref).argmin() # Get flux close to Eref
-        fluxref = grb.fluxval[:,idxref] # Obtain time spectrum for that energy
-        idtfmax, _ = flux_peak_time(grb,Eref=Eref)
-        fluxmax   = grb.fluxval[idtfmax][idxref]
+            for i in range(0, itmax+1):
+                for j in range(0, jEmax+1):
+                    fluxval[i][j] = flux[j][i]
+            fluxes.append(fluxval)
 
-        with quantity_support():
-            ax.plot(times,dts,marker=".",alpha=0.5)
-            ax.grid(ls="--",lw=1)
-            ax.set_ylabel("$\Delta t$ (" + ax.get_ylabel()+")")
-            ax.set_xscale("log")
-            ax.set_xlabel(None)
+        cls.fluxes = np.array(fluxes)*flux_unit
+        cls.Fpeaks = np.array(fpeaks)*Fpeak.unit
 
-        axx = ax.twinx()
+        return cls
 
-        label = f"{grb.id:s} \n {grb.Eval[idxref].value:5.2f} {Eref.unit:s}"
-        with quantity_support():
-            axx.plot(times,fluxref[1:],color="red",
-                     ls="-",label=label,marker=".",alpha=0.5)
+    # ##-----------------------------------------------------------------------
+    def FluxPeakTime(self, Eref, debug=True):
+        """
+        Get the maximal flux in time at the reference energy.
 
-        axx.axhline(y=fluxmax/10,color="tab:green",label="10%",alpha=0.5)
-        axx.axhline(y=fluxmax/100,color="tab:orange",label="1%",alpha=0.5)
-        axx.axvline(x=1*u.d,label="Day 1",color="black",ls=":")
-        axx.axvline(x=2*u.d,label="Day 2",color="darkgrey",ls=":")
-        axx.axvline(x=3*u.d,label="Day 3",color="lightgrey",ls=":")
-        axx.set_yscale("log")
-        single_legend(axx, bbox_to_anchor=[1.15,1])
-        plt.tight_layout()
+        Parameters
+        ----------
+        Eref : Astropy Quantity, optional
+            Reference energy. The default is 100*u.GeV.
+
+        Returns
+        -------
+        idmax : integer
+            Time index of the maximal flux.
+        idxref : integer
+            Energy index of teh reference energy.
+
+        """
+        # Get energy closest to Eref
+        idxref = np.abs(self.Eval.to(Eref.unit).value - Eref.value).argmin()
+        if debug:
+            print(32*"-")
+            print(f" Closest energy ({Eref:}) : idx={idxref:}, "
+                  f"E={self.Eval[idxref]:5.2f}")
+        # Loop over the stored flux
+
+        flx_max = []
+        t_flx_max = []
+        for item, flx in enumerate(self.fluxes):
+            # Obtain time spectrum for that energy
+            fluxref = flx[:, idxref]
+
+            # Get the index of maximal flux
+            idmax = fluxref.argmax()  # Get time index of max value
+
+            if debug:
+                print(f" Maximal flux {item:5d}: {fluxref[idmax]:5.2e} "
+                      f"@index {idmax:2d} tmax={self.tval[idmax]:5.2f}")
+
+            flx_max.append(fluxref[idmax].value)
+            t_flx_max.append(self.tval[idmax].value)
+
+        self.flux_max = np.array(flx_max)*fluxref.unit
+        self.t_flux_max = np.array(t_flx_max)*self.tval.unit
+
+    # ##-----------------------------------------------------------------------
+    def dump_to_file(self, filename):
+        """Dimp class instance to disk."""
+        with open(filename, "wb") as outfile:
+            pickle.dump(self, outfile)
+            print(" Class instance was dumped to ", filename)
+
+    # ##-----------------------------------------------------------------------
+    @classmethod
+    def read_from_disk(cls, filename):
+        """Read class instance from disk."""
+        with open(Path(filename), "rb") as f:
+            cls = pickle.load(f)
+        return cls
+
 
 ###############################################################################
 if __name__ == "__main__":
 
+    sys.path.append("../../")
 
-    import sys
-    CODE = "../../"
-    sys.path.append(CODE)
+    outfilename = "explore_simulation_from_fits.bin"
+    save = False
 
-    # Generic output file name and folders
-    infolder    = "D:/CTA/CTAA/SoHAPPy/input/"
-    outfolder   = "D:/CTA/CTAA/SoHAPPy/output/pop_tests/"
-    grb_folder  = Path(infolder,"lightcurves/LONG_FITS/")
-    os.environ['GAMMAPY_DATA'] =infolder+r'gammapy-extra-master/datasets'
+    # Bigger texts and labels
+    import seaborn as sns
+    sns.set_context("poster")  # poster, talk, notebook, paper
 
-    outfilename = "grbpop.bin"
+    if save is True:
+        # Build a default Configuration to use the sourc_ids function
+        sys.argv = ["", "-c", "../../data/config_ref.yaml", "-f", "1"]
+        cf = Configuration.command_line()
 
+        cf.ifirst = 1
+        cf.nsrc = 10
 
-    # This flags allows creating and storing the GRB classes and/or
-    # reading them
-    store = True # Store and process immediately
-    store = False # Read a previously stored population
+        filelist = cf.source_ids(os.environ["HAPPY_IN"])
+        flxs = Fluxes.from_fits(filelist)
 
-    ### Read data from exisiting population binary or create it beforehand
-    popname = Path(outfolder, "grbpop.bin")
-    if store:## Store population
-        grbpop = get_population(folder=grb_folder)
-        outfile  = open(popname,"wb")
-        pickle.dump(grbpop,outfile)
-        outfile.close()
-        print(" Population of ",len(grbpop)," GRBs saved to : {}".format(popname))
+        print(" Constant number of bins: ")
+        print("   Energies : ", len(flxs.Eval))
+        print("   Times    : ", len(flxs.tval))
+        flxs.dump_to_file(outfilename)
+
     else:
-        outfile  = open(popname,"rb")
-        grbpop= pickle.load(outfile)
-        outfile.close()
-        print(" Population of ",len(grbpop)," GRBs read from : {}".format(popname))
+        flxs = Fluxes.read_from_disk(outfilename)
 
-    # Check binning
-    check_binning(grbpop)
+    # Plot Fpeak distribution
+    with quantity_support():
+        fig, ax = plt.subplots()
+        ax.hist(np.log10(flxs.Fpeaks.value))
+        ax.set_xlabel("log 10 Fpeak (" + str(flxs.Fpeaks[0].unit) + ")")
 
-    # Time at which the flux is maximal
-    energies = [50*u.GeV, 100*u.GeV, 200*u.GeV, 500*u.GeV]
-    flux_peak_time_all(energies)
+    # Get Maximal flux time and plot
+    bins = np.logspace(0, 5, 50)
+    energies = [50*u.GeV, 100*u.GeV, 500*u.GeV]
+    fig, axes = plt.subplots(nrows=1, ncols=len(energies),
+                             figsize=(len(energies)*10, 10), sharey=True)
 
-    # Check time intervals and spectral shape for some grbs
-    check_spectra(ifirst=0, ilast=8, Eref=100*u.GeV)
+    axes[0].set_ylabel("Counts")
+    for Eref, ax in zip(energies, axes):
+        flxs.FluxPeakTime(Eref=Eref)
+        with quantity_support():
+            _, bins, _ = ax.hist(flxs.t_flux_max, bins=bins,
+                                 label=MyLabel(flxs.t_flux_max,
+                                               label=str(Eref)))
 
+            mask = np.where(flxs.Fpeaks.value >= 1)
+            ax.hist(flxs.t_flux_max[mask], bins=bins, alpha=1.0,
+                    label=MyLabel(flxs.t_flux_max[mask],
+                                  label=r"$F_{peak} > 1$"))
+
+            ax.set_xlabel(r"$T_{ \phi_{max} }$ ("
+                          + ax.get_xlabel()+")")
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+            ax.legend()
+            ax.grid(axis="both",  ls=":")
+    plt.tight_layout()
+
+    print("...completed")
